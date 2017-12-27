7969c5d718c677d30e353b6c00709babd9dbaed8
huyong007 <18168400272@163.com>
2017-12-27 23:12:18 +0800

    第五次提交

---
+++ damalianxi.js
@@ -0,0 +1,26 @@
+var name = 'xiaoming';
+var height = 170, weight = 130.1;
+var age = 18;
+var xinshengUserName = 'xiaoming';
+var wechat_user_name = 'xiaoming';
+var isMan = true;
+var student = false;
+var xinshengUserName = 'xiaoming';
+var wechat_user_name = 'xiaoming';
+var locationBase = '北京';
+var company = locationBase + '新生大学';
+var myLikeColorsList = ['blue','green','black','white'];
+var myLikeFoods = ['刀削面','拉面','牛肉面','杂酱面','麻辣面','烩面','莜面'];
+var myJavaScriptLseeionInfoObj = {
+    teacher: name,
+    teacherWeChat: wechat_user_name,
+    title:'javaScript编程入门',
+    beginTime: '2018/8/8',
+    endTime: '2018/9/4',
+    boysStudent: 417,
+    girlsStudent:119,
+    lessionTarget: '帮助更多小白学习JavaScript',
+    githubUrl:'http://github.com/xugy0926/getting-started-with-javascript'};
+    
+    console.log('姓名');
+    console.log(name);
