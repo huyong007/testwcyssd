5e618da61176a4beab3909089fe61e388b22fbf3
huyong007 <18168400272@163.com>
2018-01-16 23:49:19 +0800

    第一次提交

--- xiaodaima.js
+++ xiaodaima.js
@@ -1,22 +1,24 @@
+function soldierA(){
+    console.log('1到');
+}
+function soldierB(){
+    console.log('2到');
+}
+function soldierC(){
+    console.log('3到');
+}
+function soldierD(){
+    console.log("4到");
+}
+function soldierE(){
+    console.log("5到");
+}
+function startSoundOff(){
+    soldierA();
+    soldierB();
+    soldierC();
+    soldierD();
+    soldierE();
 
-
-var myLikeFoods = ['刀削面','拉面','杂酱面','麻辣面','牛肉面',
-'烩面','莜面'];
-
-for(var i = 0,j = myLikeFoods.length-1;
-              i < myLikeFoods.length/2 -1;
-    i++,j--){
-    console.log(myLikeFoods[i])
-    console.log(myLikeFoods[j])
-
-};
-
-console.log('\n\n\n');
-console.log('-------------------------');
-for(var i = 0,j = 1;
-    i < myLikeFoods.length/2-1 ;
-i++,j++){
-console.log('种类' +j),
-console.log(myLikeFoods[i])
-
-};
+}
+  startSoundOff();
