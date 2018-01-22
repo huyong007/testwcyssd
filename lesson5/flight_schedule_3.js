21089b33bc2032b1978d8e4861918a1910678ea7
huyong007 <18168400272@163.com>
2018-01-22 22:13:05 +0800

    参数式函数的真正理解

---
+++ lesson5/flight_schedule_3.js
@@ -0,0 +1,21 @@
+
+
+function checkFlightSchedule() {
+    console.log('开始查询航班信息');
+
+    setTimeout(function () {
+        var scheduleInfo = {
+            start: '8/23 8:00',
+            end: '8/23 10:30',
+            from: 'beijing',
+            to: 'hangzhou'
+        };
+        callback(scheduleInfo.start);
+    }, 5000);
+
+}
+function callback(startTime) {
+    console.log('通过反馈通道获得了信息');
+    console.log(startTime);
+}
+checkFlightSchedule(callback);
---
+++ lesson5/indirect.js
@@ -0,0 +1,11 @@
+function callback(){
+    console.log('我跑完了');
+}
+
+function run(fd){
+    console.log('舥哦不舥哦不');
+    fd();
+}
+
+
+run(callback);
