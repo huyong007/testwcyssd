4aeec6b0b92da1cad977e3b2b6f3461cd5284397
huyong007 <18168400272@163.com>
2018-02-04 23:30:04 +0800

    lesson7

---
+++ lesson7/homework_sample.js
@@ -0,0 +1,5 @@
+var fs = require('fs');
+var jsonfile = require('jsonfile');
+var pathString = '/Users/youngxu/allin/tem';
+var writePathString = './all_words.json';
+var errorPathString = './error_data.json';
