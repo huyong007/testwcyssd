fd6128ae1c0568959164caab6aec5f403d00dc36
huyong007 <18168400272@163.com>
2018-02-09 18:58:00 +0800

    1

--- lesson6/BMB.js
+++ lesson6/BMB.js
@@ -1,3 +1,3 @@
 //案例一：var fs = require('fs');
-fs.writeFileSync((__dirname)+'/fileNameCollection.txt', fs.readdirSync(__dirname));
+fs.writeFileSync((__dirname) + '/fileNameCollection.txt', fs.readdirSync(__dirname));
 
