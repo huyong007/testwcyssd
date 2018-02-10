210e9ad8eccb665a920591b394306cdda1d0cc52
huyong007 <18168400272@163.com>
2018-02-10 16:33:37 +0800

    未知代码

--- lesson7/xiao_sample.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-var dirPathString = '/Users/胡永/work/testwcyssd/lesson7'
-var filePathString = './result.txt'
-fs.readdir(dirPathString, function (err, obj) {
-console.dir(obj);
-    return 'obj'
-})
-var result = fs.readdir()
-fs.writeFile(filePathString, result);
