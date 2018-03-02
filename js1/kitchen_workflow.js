9847d62c4f526af5f0b68d1b4c5fe0a5768fc2bb
huyong007 <18168400272@163.com>
2018-03-02 21:07:28 +0800

    2

---
+++ js1/kitchen_workflow.js
@@ -0,0 +1,33 @@
+
+function buyFoods(){
+  console.log('采购原材料和菜');
+  var foodsList=['fish','egg','meat'];
+  return foodsList;
+}
+
+function prepare(){
+  console.log('努力磨刀...努力洗碗...努力打扫...劳动最光荣');
+  return 'Ok';
+}
+
+function cooking(foodsList){
+  console.log('今天要做这些菜'+foodsList);
+  console.log('大工告成，上菜');
+  var feast=['鸡蛋西红柿','红烧肉','红烧鱼'];
+  return feast;
+}
+
+function startWork(){
+  var success=prepare();
+  if(success){
+    var foodsList=buyFoods();
+    var feast=cooking(foodsList);
+    console.log('---酒席准备好了-----');
+    for(var i=0;i<feast.length;i++){
+      console.log(feast[i]);
+    }
+  }else{
+    console.log('还没准备好');
+  }
+}
+startWork();
--- js1/sample.js
+++
@@ -1,6 +0,0 @@
-﻿
-{x=Math.PI;
-  cx=Math.cos(x);
-  console.log("cos(π)="+cx);
-}
-
