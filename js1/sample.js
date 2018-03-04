f19b46d6910bf3a8d4c39b8a46319b45f27724bb
huyong007 <18168400272@163.com>
2018-03-04 21:19:43 +0800

    5

--- js1/sample.js
+++ js1/sample.js
@@ -1,18 +1,15 @@
+var fs= require('fs');
+var dirPathString='../../../../';
+var filePathString='../ros.js/allFilesName.txt';
 
-function checkFlightSchedule(){
-    console.log('开始查询航班信息');
-    setTimeout(function(){
-        var scheduleInfo={
-            start:'8/23 8:00',
-            end:'8/23 10:30',
-            from:'beijing',
-            to:'hangzhou'
-        };
-        callback(scheduleInfo.start);
-    },5000);
+var files=fs.readdirSync(dirPathString);
+function callback(err){
+    if(err){
+        console.log('写文件失败');
+    }else{
+        console.log('写文件成功');
+    }
 }
-function callback(startTime){
-    console.log('通过反馈通道获得信息');
-    console.log(startTime);
-}
-checkFlightSchedule();
+fs.writeFileSync(filePathString,files,callback());
+
+
